b2deaadf734dcffb200074f997d5b2134762242f
perronosaurio <waxtz@hotmail.com>
2019-07-07 18:59:16 -0400

    first testing yay!

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 const { Client } = require('discord.js')
+
 const client = new Client()
+
+client.on('ready', async () => {
+	const test = await search('reggaeton')
+	console.log(test.stations[0])
+	const url = await streamURL(test.stations[0])
+	console.log(url)
+	client.channels.get('330739726321713153').send(url)
+})
 
 client.login(process.env.TOKEN)
